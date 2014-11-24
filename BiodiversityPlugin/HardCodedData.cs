e96b6267a48fc83174088afff546292daf12c99a
Michael Degan <michael.degan@pnnl.gov>
2014-11-24 21:42:21 +0000

    LoadOrganisms now has hard coded data.

--- BiodiversityPlugin/HardCodedData.cs
+++ BiodiversityPlugin/HardCodedData.cs
@@ -14,6 +14,31 @@ namespace BiodiversityPlugin
         {
             var toReturn = new List<OrgPhylum>();
 
+            var phylumClasses = new List<OrgClass>();
+
+            var organisms = new List<Organism>();
+            organisms.Add(new Organism("Bacillus_subtilis_168", 1234)); // NOT ACTUAL TAXON
+            organisms.Add(new Organism("Staphylococcus_aureus_ATCC33591", 1235)); // NOT ACTUAL TAXON
+            organisms.Add(new Organism("Bacillus_anthracis_Sterne", 1236)); // NOT ACTUAL TAXON
+            organisms.Add(new Organism("Bacillus_anthracis_Ames", 1237)); // NOT ACTUAL TAXON
+
+            phylumClasses.Add(new OrgClass("Bacilli", organisms));
+            
+            organisms = new List<Organism>();
+            organisms.Add(new Organism("Clostridium_thermocellum", 1238)); // NOT ACTUAL TAXON
+            organisms.Add(new Organism("Dethiosulfovibrio_peptidovorans_DSM_11002", 1214)); // NOT ACTUAL TAXON
+            organisms.Add(new Organism("Heliobacterium_modesticaldum", 1244)); // NOT ACTUAL TAXON
+
+            phylumClasses.Add(new OrgClass("Clostridia", organisms));
+
+            toReturn.Add(new OrgPhylum("Firmicutes", phylumClasses));
+
+            phylumClasses = new List<OrgClass>();
+            organisms = new List<Organism>();
+            organisms.Add(new Organism("Haloferax_volcanii", 1111)); // NOT ACTUAL TAXON
+            phylumClasses.Add(new OrgClass("Halobacteria", organisms));
+            toReturn.Add(new OrgPhylum("Euryarchaeota", phylumClasses));
+
             throw new NotImplementedException();
         }
 
