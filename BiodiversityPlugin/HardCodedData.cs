9b87ec72563d212bf05710e7710a93966b99ee1c
Michael Degan <michael.degan@pnnl.gov>
2014-11-24 21:32:38 +0000

    Hey! HardCodedData.cs! PropertyGroups have starting data

--- BiodiversityPlugin/HardCodedData.cs
+++ BiodiversityPlugin/HardCodedData.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using BiodiversityPlugin.Models;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BiodiversityPlugin
 {
-    class HardCodedData
+    class HardCodedData:IDataAccess
     {
+
+        public List<OrgPhylum> LoadOrganisms()
+        {
+            var toReturn = new List<OrgPhylum>();
+
+            throw new NotImplementedException();
+        }
+
+        public List<PathwayGroup> LoadPathways()
+        {
+
+            var pathways = new List<Pathway>();
+            pathways.Add(new Pathway("Glycolysis", 00010));
+            pathways.Add(new Pathway("Citrate Cycle (TCA cycle)", 00020));
+
+            var toReturn = new List<PathwayGroup>();
+
+            var group = new PathwayGroup("Carbohydrate metabolism", pathways);
+
+            toReturn.Add(group);
+            return toReturn;
+        }
     }
 }
--- BiodiversityPlugin/IDataAccess.cs
+++ BiodiversityPlugin/IDataAccess.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using BiodiversityPlugin.Models;
 
 namespace BiodiversityPlugin
 {
     public interface IDataAccess
     {
-        void LoadData();
+        List<OrgPhylum> LoadOrganisms();
+
+        List<PathwayGroup> LoadPathways();
     }
 }
