a15c4b2cb3c9b36301e41bea4242e4350319b956
Chris Wilkins <Christopher.Wilkins@pnnl.gov>
2014-11-25 22:04:07 +0000

    Adding orgcode text coloring (doesn't actually work yet)

---
+++ BiodiversityPlugin/OrgCodeToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BiodiversityPlugin
+{
+    public class OrgCodeToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string formatted = "";
+            Color color = Colors.Black;
+            formatted = value as string;
+            if (String.IsNullOrEmpty(formatted))
+            {
+                color = Colors.Red;
+            }
+            return color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
