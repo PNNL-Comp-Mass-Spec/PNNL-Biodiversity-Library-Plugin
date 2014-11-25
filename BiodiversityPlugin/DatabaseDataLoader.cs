482b22382701d1c7fd68d97e54a6eb58ffda0288
Chris Wilkins <Christopher.Wilkins@pnnl.gov>
2014-11-25 19:50:49 +0000

    Adding DatabaseDataLoader

---
+++ BiodiversityPlugin/DatabaseDataLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiodiversityPlugin
+{
+    public class DatabaseDataLoader: IDataAccess
+    {
+        public List<Models.OrgPhylum> LoadOrganisms(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Models.PathwayGroup> LoadPathways(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
