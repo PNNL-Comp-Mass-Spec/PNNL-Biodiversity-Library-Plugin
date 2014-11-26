059d75b4ac5a71823839864931ec590eaed580f4
Michael Degan <michael.degan@pnnl.gov>
2014-11-26 19:32:39 +0000

    I forgot to add new file

---
+++ BiodiversityPlugin/Models/ProteinInformation.cs
@@ -0,0 +1,18 @@
+﻿namespace BiodiversityPlugin.Models
+{
+    public class ProteinInformation
+    {
+        public string Name { get; set; }
+
+        public string Accession { get; set; }
+
+        public string Description { get; set; }
+
+        public ProteinInformation(string name, string description, string accession)
+        {
+            Name = name;
+            Accession = accession;
+            Description = description;
+        }
+    }
+}
