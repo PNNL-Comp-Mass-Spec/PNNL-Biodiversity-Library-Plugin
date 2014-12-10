5d79af2f42379cdf553125639a53ab3337e13581
Michael Degan <michael.degan@pnnl.gov>
2014-12-10 20:13:39 +0000

    Forgot to commit file

---
+++ BiodiversityPlugin/Models/AccessionsFromDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiodiversityPlugin.Models
+{
+    public class AccessionsFromDatabase
+    {
+        private string m_keggPathId;
+        private string m_keggOrgCode;
+        private string m_refSeqId;
+        private string m_refSeqIdVersioned;
+        private string m_product;
+        private string m_note;
+
+
+    }
+}
