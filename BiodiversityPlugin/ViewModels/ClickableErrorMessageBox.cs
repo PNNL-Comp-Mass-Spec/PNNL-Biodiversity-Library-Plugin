cf14ef6683c3fc2481a0051ed5410e589f2fa56d
Michael Degan <michael.degan@pnnl.gov>
2015-06-19 19:58:24 +0000

    Had forgotten to include my new classes

---
+++ BiodiversityPlugin/ViewModels/ClickableErrorMessageBox.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace BiodiversityPlugin.ViewModels
+{
+    public class ClickableErrorMessageBox :ViewModelBase
+    {
+        private string _errorLink;
+        private string _errorLogLocation;
+
+        public RelayCommand OpenFolderCommand { get; private set; }
+
+        public ClickableErrorMessageBox(string errorLocation)
+        {
+            ErrorLogLocation = errorLocation;
+            OpenFolderCommand = new RelayCommand(OpenFolder);
+        }
+
+        public string ErrorLogLocation
+        {
+            get
+            {
+                return _errorLogLocation;;
+            }
+            set
+            {
+                _errorLogLocation = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string ErrorLink
+        {
+            get
+            {
+                return _errorLink;
+            }
+            set
+            {
+                _errorLink = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private void OpenFolder()
+        {
+            Process.Start(ErrorLogLocation);
+        }
+
+    }
+}
